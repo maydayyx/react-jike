c18ce6bf0d0e52c7c67d7c9f027f09dcc49a553a
叶星 <2043494912@qq.com>
2024-06-25 21:04:20 +0800

    初始化仓库

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Button} from "antd";
+
+function App() {
+    return (
+        <div className='app'>
+            <Button type='primary'>button</Button>
+        </div>
+    )
+}
+
+export default App
---
+++ src/router/index.jsx
@@ -0,0 +1,19 @@
+//路由配置
+import Layout from "@/pages/Layout";
+import Login from  '@/pages/Login'
+
+//配置路由实例
+import {createBrowserRouter} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Layout/>,
+    },
+    {
+        path:'/login',
+        element:<Login/>,
+    },
+])
+
+export default router
