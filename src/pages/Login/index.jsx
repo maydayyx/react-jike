72f0d0386234a974e781ea6511809381d599ad21
叶星 <2043494912@qq.com>
2024-06-25 21:57:08 +0800

    表单数据收集

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,12 +3,15 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+    const onFinsh = (value) => {
+        console.log(value)
+    }
     return (
         <div className="login">
             <Card className="login-container">
                 <img className="login-logo" src={logo} alt="" />
                 {/* 登录表单 */}
-                <Form validateTrigger='onBlur'>
+                <Form validateTrigger='onBlur' onFinish={onFinsh}>
                     <Form.Item
                         name="mobile"
                         rules={[
