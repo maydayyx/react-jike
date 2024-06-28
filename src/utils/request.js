4e4ee6775379cc3f1654762609efddc2a5a5b856
叶星 <2043494912@qq.com>
2024-06-28 21:07:07 +0800

    token失效处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,26 +1,34 @@
 import axios from "axios";
-import {getToken} from "@/utils/token.js";
+import {getToken, removeToken} from "@/utils/token.js";
+import router from "@/router/index.jsx";
+import {message} from "antd";
 // 根域名配置
 // 超时时间
 // 请求响应拦截器
 const $f = axios.create({
-    baseURL:'http://geek.itheima.net/v1_0',
-    timeout:5000
+    baseURL: 'http://geek.itheima.net/v1_0',
+    timeout: 5000
 })
 
 // 请求拦截器
-$f.interceptors.request.use((config)=>{
-   const token = getToken()
-   if(token) config.headers.Authorization = `Bearer ${token}`
+$f.interceptors.request.use((config) => {
+    const token = getToken()
+    if (token) config.headers.Authorization = `Bearer ${token}`
     return config
-},(error)=>{
+}, (error) => {
     return Promise.reject(error)
 })
 
 // 响应拦截器
-$f.interceptors.response.use((response)=>{
+$f.interceptors.response.use((response) => {
     return response.data
-},(error)=>{
+}, async (error) => {
+    switch (error.response.status) {
+        case 401 :
+            removeToken()
+            await message.error('Token失效')
+            router.navigate('/login')
+    }
     return Promise.reject(error)
 })
 
