0329fc671f04667f49c63bc90855d6e0308d20bb
叶星 <2043494912@qq.com>
2024-06-30 13:48:53 +0800

    富文本编辑器

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import {Provider} from 'react-redux'
 import store from "@/store/index.js";
 import 'normalize.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
      <Provider store={store}>
          <RouterProvider router={router} />
      </Provider>
- </React.StrictMode>
 )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
 import BarChart from "@/pages/Home/components/BarChart.jsx";
-
+import './index.scss'
 const Home = () => {
     return (
-        <div>
+        <div className='bar-box'>
             <BarChart title={'三大框架满意度'}/>
             <BarChart title={'三大框架使用度'}/>
         </div>
